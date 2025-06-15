5cbcfe6da0c21ed96689c6aa19a5b270150bcee0
wakamonoo <joven.serdanbataller21@gmail.com>
2025-06-16 00:40:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const loader = document.getElementById("loader");
   loader.classList.add("opacity-0", "transition-opacity", "duration-700");
   setTimeout(() => loader.remove(), 700); // remove after fade-out
