391911190de98f8cd99fa0ff80d4dae068ec256b
wakamonoo <joven.serdanbataller21@gmail.com>
2025-06-15 17:55:42 +0800

    asdad

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  loader.classList.add("opacity-0", "transition-opacity", "duration-700");
+  setTimeout(() => loader.remove(), 700); // remove after fade-out
+});
